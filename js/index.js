2376db16cfc9a0a5c800f2adfc42bf530dcc46bc
Elise Erickson <elisece717@gmail.com>
2023-12-05 13:41:49 -0500

    fetches and renders repos based on given user

--- js/index.js
+++ js/index.js
@@ -11,14 +11,13 @@ handleFormSubmission = (event) => {
     })
     .then(res => res.json())
     .then(usersData => {
+        document.getElementById("user-list").textContent = '';
         usersData.items.forEach( user => createUserCard(user) )
     })
     form.reset();
 }
 
 createUserCard = (user) => {
-    console.log(user);
-
     const card = document.createElement('div');
     card.classList.add("user-card");
 
@@ -32,19 +31,61 @@ createUserCard = (user) => {
     card.appendChild(avatar);
 
     const linkWrap = document.createElement("a")
-    const button = document.createElement("button")
+    const profileButton = document.createElement("button")
     linkWrap.href = user.html_url;
-    button.textContent = "See Profile"
-    linkWrap.appendChild(button);
+    profileButton.textContent = "See Profile"
+    linkWrap.appendChild(profileButton);
     card.appendChild(linkWrap);
 
-    console.log(card);
+    const repoButton = document.createElement("button")
+    repoButton.textContent = "Repos"
+    repoButton.addEventListener("click", () => fetchUserRepos(user))
+    card.appendChild(repoButton);
 
     document.getElementById("user-list").appendChild(card);
 }
 
+createRepoCard = (repo) => {
+    console.log(repo)
+    const card = document.createElement('div');
+    card.classList.add("repo-card");
+
+    const nameHeader = document.createElement("h3")
+    nameHeader.textContent = repo.name;
+    card.appendChild(nameHeader);
+
+    const description = document.createElement("p")
+    description.textContent = repo.description;
+    description.style.maxHeight = "170px";
+    card.appendChild(description);
+
+    const linkWrap = document.createElement("a")
+    const button = document.createElement("button")
+    linkWrap.href = repo.html_url;
+    button.textContent = "See Repo"
+    linkWrap.appendChild(button);
+    card.appendChild(linkWrap);
+
+    document.getElementById("repos-list").appendChild(card);
+}
+
 fetchUserRepos = (user) => {
-    
+    const repoList = document.getElementById("repos-list")
+    repoList.textContent = '';
+
+    fetch(`https://api.github.com/users/${user.login}/repos`, {
+        headers: {
+            "Accept": "application/vnd.github.v3+json"
+        }
+    })
+    .then(res => res.json())
+    .then(repoData => {
+        repoData.forEach( repo => createRepoCard(repo))
+    })
+
+    const nameHeader = document.createElement("h2")
+    nameHeader.textContent = user.login;
+    repoList.appendChild(nameHeader);
 }
 
 document.addEventListener('DOMContentLoaded', () => init())
